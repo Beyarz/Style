28e5433872eebae9f3530665c1f1fbccc529bbcc
Beyar <beyar-123@hotmail.com>
2020-06-18 18:16:33 +0200

    Refactored the items structure

--- src/scripts/card.ts
+++ src/scripts/card.ts
@@ -1,4 +1,10 @@
+interface ItemsProperty {
+  src: Array<string>,
+  id: Array<string>
+}
+
 const unOrderedList: HTMLCollection = document.getElementsByClassName('list-group list-group-horizontal')
+const items: ItemsProperty = require('../assets/items.json')
 
 function newCard (itemSource: URL, id: string): string {
   return `<li class="list-group-item no-border">
@@ -11,17 +17,19 @@ function newCard (itemSource: URL, id: string): string {
           </li>`
 }
 
-export function addButtonEventListeners (): void {
-  const selectorRadioButton: HTMLCollection = document.getElementsByClassName('no-radio')
+function structureItems (contents: ItemsProperty): void {
+  const entries = Object.entries(contents)
+  const entriesArrayLength = entries.length
 
-  for (let index = 0; index < selectorRadioButton.length; index++) {
-    const radio = selectorRadioButton.item(index)
-    radio.addEventListener('click', event => {
-      const chosenItem: string = event.target.dataset.id
-      console.log(chosenItem)
-    })
-  }
+  // entries[X] iterate over each item
+  // entries[X][X+1] properties
+  const firstItem: Array<string> = entries[0]
+  const firstItemProperty: ItemsProperty = entries[0][1]
+  const firstItemPropertySrc: Object = firstItemProperty.src
+  const firstItemPropertyId: Object = firstItemProperty.id
 }
+
+structureItems(items)
 
 export function generateItems (): void {
   const itemSource: Array<URL> = []
@@ -32,5 +40,17 @@ export function generateItems (): void {
     for (let collection = 0; collection < ulListLength; collection++) {
       unOrderedList.item(collection).innerHTML += newCard(itemSource[index], id[index])
     }
+  }
+}
+
+export function addButtonEventListeners (): void {
+  const selectorRadioButton: HTMLCollection = document.getElementsByClassName('no-radio')
+
+  for (let index = 0; index < selectorRadioButton.length; index++) {
+    const radio = selectorRadioButton.item(index)
+    radio.addEventListener('click', (event: Event) => {
+      const chosenItem: string = event.target.dataset.id
+      console.log(chosenItem)
+    })
   }
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,5 @@
 import '../styles/main.scss'
 import { generateItems, addButtonEventListeners } from './card'
-const items = require('../assets/items.json')
 
 generateItems()
 addButtonEventListeners()
